141bc03da15b4b07286393415ad719a9b832e8aa
Martin Sumera <sumera.martin@gmail.com>
2018-09-25 14:03:46 +0200

    Emoji to restaurants

--- restaurants.py
+++ restaurants.py
@@ -3,21 +3,25 @@ class Restaurant:
     pass
 
 class RestaurantEmpty(Restaurant):
-    def __init__(self, title):
+    def __init__(self, title, emoji):
         self.title = title
+        self.emoji = emoji
 
 class RestaurantURL(Restaurant):
-    def __init__(self, title, url):
+    def __init__(self, title, emoji, url):
         self.title = title
+        self.emoji = emoji
         self.url = url
 
 class RestaurantZomato(Restaurant):
-    def __init__(self, title, zomatoId):
+    def __init__(self, title, emoji, zomatoId):
         self.title = title
+        self.emoji = emoji
         self.zomatoId = zomatoId
 
 class RestaurantXPath(Restaurant):
-    def __init__(self, title, xPath):
+    def __init__(self, title, emoji, xPath):
         self.title = title
+        self.emoji = emoji
         self.xPath = xPath
     
