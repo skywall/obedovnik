15da2df0a8c2ef08604dc2a8d867d26fb8e63c55
Marek Šabo <marek.sabo@thefuntasty.com>
2018-09-25 13:58:17 +0200

    Zomato endpoint

---
+++ main.py
@@ -0,0 +1,6 @@
+from zomato import Zomato
+
+zomato = Zomato()
+for id in [16505875, 17807429, 16506578, 17807425, 18335377, 18335377, 16506939, 18126190, 16506673]:
+    restaurant = zomato.get_restaurant(id)
+    print(", ".join(str(x) for x in restaurant))
