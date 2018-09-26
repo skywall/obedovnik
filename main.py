1c894cecf1cddbcdac00de5efd1ce66d6a77877a
Lukas Sztefek <lukas.sztefek@gmail.com>
2018-09-26 08:50:34 +0200

    Catch all exceptions thrown by app to make sure dynos are not restarted

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from slack import SlackBot
 from parser import parse_restaurants
+from slack import SlackBot
 
 restaurants = parse_restaurants()
 bot = SlackBot()
 
+try:
+    ## show menus
+    for r in restaurants:
+        print(r.post_daily_menu_to_slack(bot))
 
-## show menus
-for r in restaurants:
-    print(r.post_daily_menu_to_slack(bot))
 
-## send poll
-max = 10
-for i in range(0, len(restaurants), max):
-    bot.send_poll("Poll title", restaurants[i:i + max])
-
+    ## send poll
+    max = 10
+    for i in range(0, len(restaurants), max):
+        bot.send_poll("Poll", restaurants[i:i + max])
+except Exception as exc:
+    print(type(exc))
